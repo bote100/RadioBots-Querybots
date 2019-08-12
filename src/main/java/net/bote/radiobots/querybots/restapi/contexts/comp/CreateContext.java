0123643cc966ffcbbb1054960bb19a3fa083335a
Elias Arndt <bote100officale@gmail.com>
2019-08-12 19:13:32 +0200

    fix

--- src/main/java/net/bote/radiobots/querybots/restapi/contexts/comp/CreateContext.java
+++ src/main/java/net/bote/radiobots/querybots/restapi/contexts/comp/CreateContext.java
@@ -30,23 +30,24 @@ public class CreateContext extends RestAPIContext {
     @Override
     public void handle(HttpExchange hex) {
 
-        if(!checkParams(hex)) return;
+        if (!checkParams(hex)) return;
 
         int botid = -1;
 
         try {
             QueryBotApplication.getInstance().getMysqlConnection().createStatement().executeUpdate(
                     "INSERT INTO query_bot_entity (apikey, modules, query_password, query_user, server, nickname) VALUES " +
-                            "('"+getHeaderVal(hex, "apikey")+"', '[\"welcome\"]', '"+getHeaderVal(hex, "querypw")+"'," +
-                            "'"+getHeaderVal(hex, "queryuser")+"', '"+getHeaderVal(hex, "server")+"', '"+getHeaderVal(hex, "nickname")+"')"
+                            "('" + getHeaderVal(hex, "apikey") + "', '[\"welcome\"]', '" + getHeaderVal(hex, "querypw") + "'," +
+                            "'" + getHeaderVal(hex, "queryuser") + "', '" + getHeaderVal(hex, "server") + "', '" + getHeaderVal(hex, "nickname") + "')"
             );
 
-            ResultSet set = QueryBotApplication.getInstance().getMysqlConnection().createStatement().executeQuery(
-                    "SELECT uuid FROM query_bot_entity ORDER BY uuid DESC LIMIT 1"
-            );
+            ResultSet set = query("SELECT uuid FROM query_bot_entity ORDER BY uuid DESC LIMIT 1");
+            botid = set.getInt("uuid");
 
-            if(set.next())
-                botid = set.getInt("uuid");
+            update("INSERT INTO afk_poke_entity (id, message) VALUES ('"+botid+"', 'Du wurdest in den AFK Channel, aufgrund zu langer Inaktivität, verschoben!')");
+            update("INSERT INTO welcome_message_entity (id, message) VALUES ('"+botid+"', 'Willkommen auf diesem TeamSpeak!')");
+            update("INSERT INTO support_bot_entity (id, channel, message, tsgroup) " +
+                    "VALUES ('"+botid+"', 'Support-Channel', 'Ein Nutzer wartet im Support!', '1')");
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -57,4 +58,26 @@ public class CreateContext extends RestAPIContext {
         sendResponse(new JSONObject().put("success", true).put("bot", botid).toString(), hex);
 
     }
+
+    private ResultSet query(String sql) {
+        try {
+
+            ResultSet set = QueryBotApplication.getInstance().getMysqlConnection().createStatement().executeQuery(sql);
+            set.next();
+            return set;
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    private void update(String sql) {
+        try {
+            QueryBotApplication.getInstance().getMysqlConnection().createStatement().executeUpdate(sql);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
